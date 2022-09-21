887d64c87fe51819ebfd863dd21bdcb96528e765
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-09-21 10:33:35 +0100

    Add follow and unfollow functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -13,6 +13,8 @@ import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
+import PopularProfiles from "../profiles/PopularProfiles";
+
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
@@ -41,7 +43,7 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
           {currentUser ? (
@@ -67,7 +69,7 @@ function PostPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
