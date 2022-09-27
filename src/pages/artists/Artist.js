1982d3625a098802c926eb7460714c2d4fd04234
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-09-27 01:32:35 +0100

    Update height of Avatar in artists page

--- src/pages/artists/Artist.js
+++ src/pages/artists/Artist.js
@@ -37,7 +37,7 @@ const Artist = (props) => {
         {!isProfilePage && (
           <Media className="align-items-center justify-content-between">
             <Link to={`/profiles/${profile_id}`}>
-              <Avatar src={profile_image} height={55} />
+              <Avatar src={profile_image} height={100} />
               {owner}
             </Link>
           </Media>
