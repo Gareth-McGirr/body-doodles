b3a402579503085e013553ace54ca1c32efea50b
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-09-29 09:34:23 +0100

    Update styling in Posts to fix contrast error.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -76,7 +76,7 @@ const Post = (props) => {
     <Card className={styles.Post}>
       <Card.Body>
         <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
+          <Link to={`/profiles/${profile_id}`} className={styles.Link}>
             <Avatar src={profile_image} height={55} />
             {owner}
           </Link>
